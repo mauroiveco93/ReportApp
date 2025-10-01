fffa3849c5cd141292588423565d5784f52b1dd7
mauroiveco93 <mauro.forte@ivecogroup.com>
2025-10-01 11:26:08 +0200

    Update app.py

--- ReportApp/app.py
+++ ReportApp/app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from genera_report import genera_report
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LOGO_PATH = os.path.join(BASE_DIR, "logo.jpg")
+
+# Mostra logo in Streamlit
+if os.path.exists(LOGO_PATH):
+    st.image(LOGO_PATH, width=150)
 
 st.title("Engine Serial Number Report")
 
